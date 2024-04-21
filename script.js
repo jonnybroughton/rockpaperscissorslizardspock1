715cfe4becba6525dcf2702bc3eb236945be11d2
jonny broughton <jonny.broughton08@gmail.com>
2024-04-21 10:23:08 +0000

    add click event listeners

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ const userChoiceText = document.getElementById('user-choice-text');
 const computerChoiceText = document.getElementById('computer-choice-text');
 const gameResultText = document.getElementById('game-result-text');
 const restartButton = document.getElementById('restart-button');
+
+rockButton.addEventListener('click', () => playGame('rock'));
+paperButton.addEventListener('click', () => playGame('paper'));
+scissorsButton.addEventListener('click', () => playGame('scissors'));
+lizardButton.addEventListener('click', () => playGame('lizard'));
+spockButton.addEventListener('click', () => playGame('spock'));
