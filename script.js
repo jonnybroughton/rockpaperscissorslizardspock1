07761f3f0a2cbf85cb1da6410f3aaa28814ae82f
jonny broughton <jonny.broughton08@gmail.com>
2024-04-21 10:07:44 +0000

    define button constants

---
+++ script.js
@@ -0,0 +1,5 @@
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+const lizardButton = document.getElementById('lizard');
+const spockButton = document.getElementById('spock');
