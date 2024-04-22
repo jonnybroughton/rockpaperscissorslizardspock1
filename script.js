716f4266030843aaf54ce5e22e8e556958b054bd
jonny broughton <jonny.broughton08@gmail.com>
2024-04-22 10:23:11 +0000

    add more comments to further seperate content

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+/*DOMContentLoaded to make sure everything loads to ensure game plays as expected*/
 document.addEventListener("DOMContentLoaded", function() {
+    /*define constants and click event listeners*/
     const rockButton = document.getElementById('rock');
     const paperButton = document.getElementById('paper');
     const scissorsButton = document.getElementById('scissors');
@@ -14,13 +16,13 @@ document.addEventListener("DOMContentLoaded", function() {
     scissorsButton.addEventListener('click', () => playGame('scissors'));
     lizardButton.addEventListener('click', () => playGame('lizard'));
     spockButton.addEventListener('click', () => playGame('spock'));
-
+    /*Computer Choice Function*/
     function getComputerChoice(){
         const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
         const randomNumber = Math.floor(Math.random() * 5);
         return choices[randomNumber];
     }
-
+    /*Determine Winner Function*/
     function determineWinner(userChoice, computerChoice){
         if (userChoice === computerChoice){
             return 'Tie';
@@ -36,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function() {
             return 'Computer Wins';
         }
     }
-
+    /*PlayGame function from user choices*/
     function playGame(userChoice){
         const computerChoice = getComputerChoice();
         userChoiceText.textContent = capitalize(userChoice);
@@ -45,10 +47,11 @@ document.addEventListener("DOMContentLoaded", function() {
         gameResultText.textContent = result;
         updateScore(result);
     }
+    /*Function to capitalise user choice result*/
     function capitalize(word) {
         return word.charAt(0).toUpperCase() + word.slice(1);
     }
-
+    /*function to enable scoreboard functionality*/
     let userScore = 0;
     let computerScore = 0;
 
@@ -67,7 +70,7 @@ document.addEventListener("DOMContentLoaded", function() {
             checkOverallWinner();
         }
     }
-
+    /*Function to decide overall winner out of 10 games*/
     function checkOverallWinner(){
         const overallWinnerText = document.getElementById('overall-winner-text');
         if(userScore === 10){
@@ -78,7 +81,7 @@ document.addEventListener("DOMContentLoaded", function() {
             restartButton.style.display = 'block';
         }
     }
-
+    /*Function to allow game to be restarted*/
     function restartGame(){
         userScore = 0;
         computerScore = 0;
